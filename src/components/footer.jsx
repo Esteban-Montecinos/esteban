a59d118dd6c1de9a22eedb1e8088adabfbe13285
Esteban-Montecinos <estebanmontecinos2001@gmail.com>
2023-10-16 12:13:30 -0300

    padding bottom

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,7 +4,7 @@ import FooterContactos from "./footer-contactos";
 export default function Footer() {
   return (
     <footer className="mt-8 border-t border-zinc-800">
-      <nav className="flex flex-col max-w-5xl gap-4 px-8 m-auto mt-20 mb-9">
+      <nav className="flex flex-col max-w-5xl gap-4 px-8 m-auto mt-20 pb-9">
         <div className="flex flex-row flex-wrap items-center justify-center w-full gap-4">
           <a
             href="https://www.instagram.com/estebannmontecinos/"
