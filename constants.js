3607fdbd8b219b09f35848db58bc302d8466a0b8
Esteban-Montecinos <estebanmontecinos2001@gmail.com>
2023-10-24 19:16:32 -0300

    nuevos proyectos

--- constants.js
+++ constants.js
@@ -1,9 +1,8 @@
 import ico from "@/assets/ico.webp"
 import inacap from "@/assets/inacap.webp"
+import { IconBrandRedux, IconBrandTypescript } from "@tabler/icons-react";
 import {
   IconBrandGithub,
-  IconBrandLinkedin,
-  IconBrandGmail,
   IconBrandAndroid,
   IconBrandNextjs,
   IconBrandReactNative,
@@ -23,22 +22,6 @@ import {
 
 export const proyectos = [
   {
-    id: "1",
-    titulo: "Preguntas anónimas",
-    descripcion:
-      "Aplicación de preguntas anónimas, una aplicación con una interfáz similar a NGL. A diferencia de NGL, esta es una aplicación de preguntas anónimas donde todas las preguntas llegan al muro principal.",
-    git_url: "https://github.com/Esteban-Montecinos/preguntas-app",
-    web_url: "https://preguntas-app.vercel.app/",
-    etiquetas: [
-      { nombre: "NextJS" },
-      { nombre: "TailwindCSS" },
-      { nombre: "OpenGraph" },
-      { nombre: "Supabase" },
-      { nombre: "ToastifyJS" },
-      { nombre: "React" },
-    ],
-  },
-  {
     id: "2",
     titulo: "Frontend Mentor Challenges",
     descripcion:
@@ -47,50 +30,39 @@ export const proyectos = [
     web_url: "https://frontendmentor-esteban.vercel.app/",
     etiquetas: [
       { nombre: "React" },
-      { nombre: "NextJS" },
+      { nombre: "Next.js" },
       { nombre: "TailwindCSS" },
       { nombre: "Cheerio" },
     ],
   },
   {
-    id: "3",
-    titulo: "Comida al azar",
-    descripcion:
-      "Esta aplicación recupera desde una API una receta culinaria completa junto con la lista de sus ingredientes asociados.",
-    git_url: "https://github.com/Esteban-Montecinos/random-meal",
-    web_url: "https://random-meal-peach.vercel.app/",
-    etiquetas: [
-      { nombre: "React" },
-      { nombre: "NextJS" },
-      { nombre: "TailwindCSS" },
-    ],
-  },
-  {
-    id: "5",
-    titulo: "*MNTodo List",
-    descripcion:
-      "Aplicación de gestión de tareas que posibilita la creación y seguimiento de una lista de tareas pendientes, permitiendo tanto la adición de nuevas tareas como la marcación de las mismas como completadas.",
-    git_url: "https://github.com/Esteban-Montecinos/todo-list-app",
-    web_url: "https://mntodo-list.vercel.app/",
-    etiquetas: [
-      { nombre: "React" },
-      { nombre: "NextJS" },
-      { nombre: "TailwindCSS" },
-      { nombre: "Zustand" },
-    ],
-  },
-  {
     id: "6",
-    titulo: "*MNTree",
+    titulo: "Clon de Twitter | *MNTree",
     descripcion:
       "Plataforma de redes sociales que emplea el protocolo OAuth de GitHub para autenticar a los usuarios, brindando la capacidad de compartir contenido y dar 'Me gusta' a las diversas publicaciones.",
     git_url: "https://github.com/Esteban-Montecinos/mntree",
     web_url: "https://mntree.vercel.app/",
     etiquetas: [
       { nombre: "React" },
-      { nombre: "NextJS" },
+      { nombre: "Next.js" },
       { nombre: "TailwindCSS" },
       { nombre: "Supabase" },
+    ],
+  },
+  {
+    id: "1",
+    titulo: "Preguntas anónimas",
+    descripcion:
+      "Aplicación de preguntas anónimas, una aplicación con una interfáz similar a NGL. A diferencia de NGL, esta es una aplicación de preguntas anónimas donde todas las preguntas llegan al muro principal.",
+    git_url: "https://github.com/Esteban-Montecinos/preguntas-app",
+    web_url: "https://preguntas-app.vercel.app/",
+    etiquetas: [
+      { nombre: "Next.js" },
+      { nombre: "TailwindCSS" },
+      { nombre: "OpenGraph" },
+      { nombre: "Supabase" },
+      { nombre: "ToastifyJS" },
+      { nombre: "React" },
     ],
   },
   {
@@ -102,9 +74,64 @@ export const proyectos = [
     web_url: "https://steam-finder-connect.vercel.app/",
     etiquetas: [
       { nombre: "React" },
-      { nombre: "NextJS" },
+      { nombre: "Next.js" },
       { nombre: "TailwindCSS" },
       { nombre: "Cheerio" },
+    ],
+  },
+  {
+    id: "8",
+    titulo: "Calculadora de edad",
+    descripcion:
+      "Es una aplicación web moderna y fácil de usar que te permite calcular rápidamente la edad de una persona. Ya sea que necesites saber cuántos años tiene alguien, te brinda los años, meses y días exactos, la herramienta perfecta para hacerlo de manera rápida y precisa.",
+    git_url: "https://github.com/Esteban-Montecinos/age-calculator-app",
+    web_url: "https://age-calculator-app-dusky.vercel.app/",
+    etiquetas: [
+      { nombre: "React" },
+      { nombre: "Next.js" },
+      { nombre: "TailwindCSS" },
+    ],
+  },
+  {
+    id: "9",
+    titulo: "GitHub Finder",
+    descripcion:
+      "te permite buscar y explorar perfiles de desarrolladores en GitHub. Si estás interesado en conocer más acerca de los desarrolladores que utilizan esta popular plataforma de control de versiones, o si buscas talento para tu equipo, esta web utiliza la API de GitHub",
+    git_url: "https://github.com/Esteban-Montecinos/githubfinder",
+    web_url: "https://github-finder-connect.vercel.app/esteban-montecinos",
+    etiquetas: [
+      { nombre: "React" },
+      { nombre: "Next.js" },
+      { nombre: "TailwindCSS" },
+      { nombre: "OpenGraph" },
+      { nombre: "ToastifyJS" },
+    ],
+  },
+  {
+    id: "5",
+    titulo: "*MNTodo List",
+    descripcion:
+      "Aplicación de gestión de tareas que posibilita la creación y seguimiento de una lista de tareas pendientes, permitiendo tanto la adición de nuevas tareas como la marcación de las mismas como completadas.",
+    git_url: "https://github.com/Esteban-Montecinos/todo-list-app",
+    web_url: "https://mntodo-list.vercel.app/",
+    etiquetas: [
+      { nombre: "React" },
+      { nombre: "Next.js" },
+      { nombre: "TailwindCSS" },
+      { nombre: "Zustand" },
+    ],
+  },
+  {
+    id: "10",
+    titulo: "Generador de consejos",
+    descripcion:
+      "Diseño sacado de FrontendMentor challenges, el cual utiliza una API que genera consejos de manera aleatoria.",
+    git_url: "https://github.com/Esteban-Montecinos/advice-generator-app",
+    web_url: "https://advice-generator-app-three-rose.vercel.app/",
+    etiquetas: [
+      { nombre: "React" },
+      { nombre: "Next.js" },
+      { nombre: "TailwindCSS" },
     ],
   },
 ];
@@ -152,12 +179,20 @@ export const tecnologias = [
     icon: <IconBrandJavascript className="w-12 h-12" aria-label="JavaScript" stroke={1.2}/>,
   },
   {
+    nombre: "TypeScript",
+    icon: <IconBrandTypescript className="w-12 h-12" aria-label="TypeScript" stroke={1.2}/>,
+  },
+  {
     nombre: "React",
     icon: <IconBrandReact className="w-12 h-12" aria-label="React" stroke={1.2}/>,
   },
   {
     nombre: "Next",
     icon: <IconBrandNextjs className="w-12 h-12" aria-label="Next" stroke={1.2}/>,
+  },
+  {
+    nombre: "Redux",
+    icon: <IconBrandRedux className="w-12 h-12" aria-label="Next" stroke={1.2}/>,
   },
   {
     nombre: "Vite",
@@ -202,4 +237,4 @@ export const tecnologias = [
     icon: <IconBrandGithub className="w-12 h-12" aria-label="GitHub" stroke={1.2}/>,
   },
 ];
-export const cv = "https://drive.google.com/file/d/1iC8Ipp2WAWzQWqabJQgthpb4--a-dkqT/view?usp=sharing"
+export const cv = "https://drive.google.com/file/d/1JiuKnLtQy4UjXo3oFRw-JaEd6eOGJvJe/view?usp=sharing"
