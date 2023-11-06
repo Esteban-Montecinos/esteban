c6c120c9afb9ba4627123f29bd3c692af0b4368d
Esteban-Montecinos <estebanmontecinos2001@gmail.com>
2023-11-06 16:03:56 -0300

    chip style

--- src/components/proyecto-card.jsx
+++ src/components/proyecto-card.jsx
@@ -11,7 +11,9 @@ export default function ProyectoCard({
   return (
     <Card className="flex flex-col w-full p-1 transition-colors border bg-zinc-400 dark:bg-black dark:border-zinc-800 border-zinc-400 bg-gradient-to-tl dark:from-black/80 from-zinc-50 dark:via-zinc-600/20 via-zinc-200/60 dark:to-black/80 to-zinc-50 dark:hover:bg-zinc-950 hover:bg-zinc-400/60 dark:hover:border-white hover:border-black">
       <CardHeader className="px-2 pb-2">
-        <h2 className="text-2xl font-bold text-black dark:text-white">{titulo}</h2>
+        <h2 className="text-2xl font-bold text-black dark:text-white">
+          {titulo}
+        </h2>
       </CardHeader>
       <CardBody className="justify-between h-full p-2 text-small dark:text-zinc-400 text-zinc-700">
         <p>{descripcion}</p>
@@ -22,6 +24,10 @@ export default function ProyectoCard({
               variant="flat"
               size="sm"
               radius="sm"
+              classNames={{
+                base: "dark:bg-zinc-800 bg-white",
+                content: "dark:text-white text-black",
+              }}
             >
               {nombre}
             </Chip>
