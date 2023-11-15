c2fefa94f0bdfa34e1446309d1aa20d8fc6aa59c
Esteban-Montecinos <estebanmontecinos2001@gmail.com>
2023-11-14 21:08:15 -0300

    Color Accessibility Contrast Ratio

--- src/components/blog-card.jsx
+++ src/components/blog-card.jsx
@@ -1,17 +1,13 @@
-'use client'
+"use client";
 import { motion, useMotionTemplate, useMotionValue } from "framer-motion";
 import Link from "next/link";
 import { formatDate } from "../../lib/fecha-format";
 
-export default function BlogCard({date,slug, title,description}) {
-    let mouseX = useMotionValue(0);
+export default function BlogCard({ date, slug, title, description }) {
+  let mouseX = useMotionValue(0);
   let mouseY = useMotionValue(0);
 
-  function handleMouseMove({
-    currentTarget,
-    clientX,
-    clientY,
-  }) {
+  function handleMouseMove({ currentTarget, clientX, clientY }) {
     let { left, top } = currentTarget.getBoundingClientRect();
 
     mouseX.set(clientX - left);
@@ -19,11 +15,10 @@ export default function BlogCard({date,slug, title,description}) {
   }
   return (
     <article
-          onMouseMove={handleMouseMove}
-            
-            className="relative flex flex-col gap-2 p-3 transition-colors border group dark:bg-black bg-zinc-400 rounded-xl dark:border-zinc-800 border-zinc-400 bg-gradient-to-tl dark:from-black/80 from-zinc-50 dark:via-zinc-600/20 via-zinc-200/60 dark:to-black/80 to-zinc-50 dark:hover:bg-zinc-950 hover:bg-zinc-400/60 dark:hover:border-white/50 hover:border-black/50"
-          >
-          <motion.div
+      onMouseMove={handleMouseMove}
+      className="relative flex flex-col gap-2 p-3 transition-colors border group dark:bg-black bg-zinc-400 rounded-xl dark:border-zinc-800 border-zinc-400 bg-gradient-to-tl dark:from-black/80 from-zinc-50 dark:via-zinc-600/20 via-zinc-200/60 dark:to-black/80 to-zinc-50 dark:hover:bg-zinc-950 hover:bg-zinc-400/60 dark:hover:border-white/50 hover:border-black/50"
+    >
+      <motion.div
         className="absolute transition duration-300 opacity-0 pointer-events-none -inset-px rounded-xl group-hover:opacity-100"
         style={{
           background: useMotionTemplate`
@@ -35,17 +30,27 @@ export default function BlogCard({date,slug, title,description}) {
           `,
         }}
       />
-            <div className="flex flex-col h-full gap-2">
-              <time dateTime={date} className="text-sm dark:text-zinc-400 text-zinc-700">{formatDate(date)}</time>
-              <Link href={`blog/${slug}`} className="text-lg font-semibold ">{title}</Link>
-              <p className="mb-2 dark:text-zinc-200 text-zinc-800">{description}</p>
-            </div>
-            <Link
-              href={`blog/${slug}`}
-              className="flex items-center justify-center w-full p-2 text-sm transition-colors rounded-md justify-self-end dark:text-zinc-400 hover:text-black text-zinc-700 dark:bg-zinc-800/80 bg-zinc-400/60 dark:hover:text-zinc-100 dark:hover:bg-zinc-800"
-            >
-              Leer Más
-            </Link>
-          </article>
-  )
+      <div className="flex flex-col h-full gap-2">
+        <time
+          dateTime={date}
+          className="text-sm dark:text-zinc-400 text-zinc-700"
+        >
+          {formatDate(date)}
+        </time>
+        <Link href={`blog/${slug}`} className="text-lg font-semibold ">
+          {title}
+        </Link>
+        <p className="mb-2 dark:text-zinc-200 text-zinc-800">{description}</p>
+      </div>
+      <Link
+        href={`blog/${slug}`}
+        className="w-full p-2 text-sm text-center text-white transition-colors bg-black border rounded-lg border-black/50 dark:border-white/50 group/blog justify-self-end dark:text-black hover:text-black hover:bg-white dark:bg-white dark:hover:text-white dark:hover:bg-black"
+      >
+        Leer Más{" "}
+        <span className="inline-block transition-transform group-hover/blog:translate-x-1 motion-reduce:transform-none">
+          →
+        </span>
+      </Link>
+    </article>
+  );
 }
--- src/components/proyecto-card.jsx
+++ src/components/proyecto-card.jsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { motion, useMotionTemplate, useMotionValue } from "framer-motion";
 import { Card, CardBody, CardFooter, CardHeader } from "@nextui-org/card";
 import { Chip } from "@nextui-org/chip";
@@ -13,18 +13,17 @@ export default function ProyectoCard({
   let mouseX = useMotionValue(0);
   let mouseY = useMotionValue(0);
 
-  function handleMouseMove({
-    currentTarget,
-    clientX,
-    clientY,
-  }) {
+  function handleMouseMove({ currentTarget, clientX, clientY }) {
     let { left, top } = currentTarget.getBoundingClientRect();
 
     mouseX.set(clientX - left);
     mouseY.set(clientY - top);
   }
   return (
-    <Card onMouseMove={handleMouseMove} className="relative flex flex-col w-full p-1 transition-colors border group bg-zinc-400 dark:bg-black bg-gradient-to-tl dark:from-black/80 from-zinc-50 dark:via-zinc-600/20 via-zinc-200/60 dark:to-black/80 to-zinc-50 dark:hover:bg-zinc-950 hover:bg-zinc-400/60 dark:border-zinc-800 border-zinc-400 hover:dark:border-white/80 hover:border-black/80">
+    <Card
+      onMouseMove={handleMouseMove}
+      className="relative flex flex-col w-full p-1 transition-colors border group bg-zinc-400 dark:bg-black bg-gradient-to-tl dark:from-black/80 from-zinc-50 dark:via-zinc-600/20 via-zinc-200/60 dark:to-black/80 to-zinc-50 dark:hover:bg-zinc-950 hover:bg-zinc-400/60 dark:border-zinc-800 border-zinc-400 hover:dark:border-white/80 hover:border-black/80"
+    >
       <motion.div
         className="absolute transition duration-300 opacity-0 pointer-events-none -inset-px rounded-xl group-hover:opacity-100"
         style={{
@@ -63,26 +62,33 @@ export default function ProyectoCard({
       </CardBody>
       <CardFooter className="justify-between gap-4 px-2">
         <a
-          className="w-full p-2 text-sm text-center transition-colors rounded-md dark:text-zinc-400 text-zinc-700 dark:bg-zinc-800 bg-zinc-400 dark:hover:text-white hover:text-black dark:hover:bg-zinc-800 hover:bg-zinc-400"
+          className="w-full p-2 text-sm text-center text-white transition-colors bg-black border rounded-lg group/demo border-black/50 dark:border-white/50 dark:text-black dark:bg-white dark:hover:text-white hover:text-black dark:hover:bg-black hover:bg-white"
           href={web_url}
           target="_blank"
           rel="noopener noreferrer"
         >
-          Demo
+          Demo{" "}
+          <span className="inline-block transition-transform group-hover/demo:translate-x-1 motion-reduce:transform-none">
+            →
+          </span>
         </a>
         <a
-          className="w-full p-2 text-sm text-center transition-colors rounded-md dark:text-zinc-400 text-zinc-700 dark:bg-zinc-800 bg-zinc-400 dark:hover:text-white hover:text-black dark:hover:bg-zinc-800 hover:bg-zinc-400"
+          className="w-full p-2 text-sm text-center text-white transition-colors bg-black border rounded-lg group/repo border-black/50 dark:border-white/50 dark:text-black dark:bg-white dark:hover:text-white hover:text-black dark:hover:bg-black hover:bg-white"
           href={git_url}
           target="_blank"
           rel="noopener noreferrer"
         >
-          Repositorio
+          Repositorio{" "}
+          <span className="inline-block transition-transform group-hover/repo:translate-x-1 motion-reduce:transform-none">
+            →
+          </span>
         </a>
       </CardFooter>
     </Card>
   );
 }
-{/* <div
+{
+  /* <div
   className="relative max-w-md px-8 py-16 bg-gray-900 border shadow-2xl group rounded-xl border-white/10"
   onMouseMove={handleMouseMove}
 >
@@ -112,4 +118,5 @@ export default function ProyectoCard({
       illum eum ullam nostrum atque quam.
     </p>
   </div>
-</div> */}
+</div> */
+}
