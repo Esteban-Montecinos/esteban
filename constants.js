19c164836294cc579c33ddb33c9858190bebf72f
Esteban-Montecinos <estebanmontecinos2001@gmail.com>
2023-11-15 16:46:19 -0300

    nuevo cv

--- constants.js
+++ constants.js
@@ -237,4 +237,4 @@ export const tecnologias = [
     icon: <IconBrandGithub className="w-12 h-12" aria-label="GitHub" stroke={1.2}/>,
   },
 ];
-export const cv = "https://drive.google.com/file/d/1DA7F9lu7xjOCXgAdiSoehyGcD2jO1NjL/view?usp=sharing"
+export const cv = "https://drive.google.com/file/d/1oFbTdYrwTCZWfdA99NrdiySDkX6Of3Hw/view?usp=sharing"
