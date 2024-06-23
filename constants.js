52d63d629d1afb610e78075aebbc5031908c2228
Esteban-Montecinos <estebanmontecinos2001@gmail.com>
2024-06-23 15:24:27 -0400

    update

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
-import { IconBrandGmail } from "@tabler/icons-react";
-import { IconBrandLinkedin, IconBrandRedux, IconBrandTypescript, IconFileCv } from "@tabler/icons-react";
+
+import { IconBrandLinkedin, IconBrandRedux, IconBrandTypescript } from "@tabler/icons-react";
 import {
   IconBrandGithub,
   IconBrandAndroid,
@@ -41,136 +41,7 @@ export const proyectos = [
       { nombre: "html-to-image" },
       { nombre: "js-base64" },
     ],
-  },
-  {
-    id: "12",
-    titulo: "3D Aim Trainer",
-    src: "/projects/aim.webm",
-    img: "/projects/aim.webp",
-    fecha: "01/02/2024",
-    descripcion:
-      "Mini juego en el que hay que disparar al mayor número de esferas en 20 segundos, este juego lo realize con el objetivo de aprender Three.js y aprendí muchas cosas sobre la iluminación en un espacio 3D, la posición y rotación de objetos, cargado de modelos, texturas y órbitas.",
-    git_url: "https://github.com/Esteban-Montecinos/3d-aim-trainer",
-    web_url: "https://3d-aim-trainer.vercel.app/",
-    etiquetas: [
-      { nombre: "React" },
-      { nombre: "Vite" },
-      { nombre: "TailwindCSS" },
-      { nombre: "Three.Js" },
-      { nombre: "Zustand" },
-      { nombre: "Cannon" },
-      { nombre: "Drei" },
-      { nombre: "Fiber" },
-    ],
-  },
-  {
-    id: "9",
-    src: "/projects/github.webm",
-    img: "/projects/github.webp",
-    fecha: "12/29/2023",
-    titulo: "Buscador Perfil GitHub",
-    descripcion:
-      "te permite buscar y explorar perfiles de desarrolladores en GitHub. Si estás interesado en conocer más acerca de los desarrolladores que utilizan esta popular plataforma de control de versiones, o si buscas talento para tu equipo, esta web utiliza la API de GitHub",
-    git_url: "https://github.com/Esteban-Montecinos/github-profile",
-    web_url: "https://buscador-perfil-github.vercel.app/Esteban-Montecinos",
-    etiquetas: [
-      { nombre: "React" },
-      { nombre: "Next.js" },
-      { nombre: "TailwindCSS" },
-      { nombre: "OpenGraph" },
-      { nombre: "Framer Motion" },
-      { nombre: "Lenis" },
-    ],
-  },
-  {
-    id: "7",
-    src: "/projects/steam.webm",
-    img: "/projects/steam.webp",
-    fecha: "08/30/2023",
-    titulo: "Steam Finder Connect",
-    descripcion:
-      "Una aplicación web desarrollada con Next.js y web scraping para proporcionar a los usuarios una interfaz amigable para buscar perfiles de usuarios en la plataforma Steam. pueden buscar perfiles con su respectivo ID de steam o por el vanity name",
-    git_url: "https://github.com/Esteban-Montecinos/steam-finder",
-    web_url: "https://steam-finder-connect.vercel.app/",
-    etiquetas: [
-      { nombre: "React" },
-      { nombre: "Next.js" },
-      { nombre: "TailwindCSS" },
-      { nombre: "Cheerio" },
-      { nombre: "Framer Motion" },
-    ],
-  },
-  {
-    id: "6",
-    src: "/projects/clonx.webm",
-    img: "/projects/clonx.webp",
-    fecha: "08/07/2023",
-    titulo: "Clon de Twitter | *MNTree",
-    descripcion:
-      "Plataforma de redes sociales que emplea el protocolo OAuth de GitHub para autenticar a los usuarios, brindando la capacidad de compartir contenido y dar 'Me gusta' a las diversas publicaciones.",
-    git_url: "https://github.com/Esteban-Montecinos/mntree",
-    web_url: "https://mntree.vercel.app/",
-    etiquetas: [
-      { nombre: "React" },
-      { nombre: "Next.js" },
-      { nombre: "TailwindCSS" },
-      { nombre: "Supabase" },
-      { nombre: "next-themes" },
-    ],
-  },
-  {
-    id: "8",
-    src: "/projects/calculadora.webm",
-    img: "/projects/calculadora.webp",
-    fecha: "07/31/2023",
-    titulo: "Calculadora de edad",
-    descripcion:
-      "Es una aplicación web moderna y fácil de usar que te permite calcular rápidamente la edad de una persona. Ya sea que necesites saber cuántos años tiene alguien, te brinda los años, meses y días exactos, la herramienta perfecta para hacerlo de manera rápida y precisa.",
-    git_url: "https://github.com/Esteban-Montecinos/age-calculator-app",
-    web_url: "https://age-calculator-app-dusky.vercel.app/",
-    etiquetas: [
-      { nombre: "React" },
-      { nombre: "Next.js" },
-      { nombre: "TailwindCSS" },
-      { nombre: "Zustand" },
-    ],
-  },
-  {
-    id: "2",
-    src: "/projects/frontendmentor.webm",
-    img: "/projects/frontendmentor.webp",
-    fecha: "07/25/2023",
-    titulo: "Frontend Mentor Challenges",
-    descripcion:
-      "Se ha desarrollado una aplicación que realiza el proceso de extracción de datos (scraping) desde el sitio web Frontend Mentor, con el propósito de obtener los retos (challenges) disponibles. Esta herramienta permite la funcionalidad de otorgar 'Me gusta' a los proyectos seleccionados para su futura realización.",
-    git_url: "https://github.com/Esteban-Montecinos/frontend-mentor-challenges",
-    web_url: "https://frontendmentor-esteban.vercel.app/",
-    etiquetas: [
-      { nombre: "React" },
-      { nombre: "Next.js" },
-      { nombre: "TailwindCSS" },
-      { nombre: "Cheerio" },
-    ],
-  },
-  {
-    id: "1",
-    src: "/projects/preguntas.webm",
-    img: "/projects/preguntas.webp",
-    fecha: "07/24/2023",
-    titulo: "Preguntas anónimas",
-    descripcion:
-      "Aplicación de preguntas anónimas, una aplicación con una interfáz similar a NGL. A diferencia de NGL, esta es una aplicación de preguntas anónimas donde todas las preguntas llegan al muro principal.",
-    git_url: "https://github.com/Esteban-Montecinos/preguntas-app",
-    web_url: "https://preguntas-app.vercel.app/",
-    etiquetas: [
-      { nombre: "Next.js" },
-      { nombre: "TailwindCSS" },
-      { nombre: "OpenGraph" },
-      { nombre: "Supabase" },
-      { nombre: "ToastifyJS" },
-      { nombre: "React" },
-    ],
-  },
+  }
 ];
 export const educacion = [
   {
@@ -178,21 +49,21 @@ export const educacion = [
     fecha: "Agosto 2021 - Diciembre 2022",
     nombre: "Inacap Sede Osorno",
     titulo: "Ingeniería En Informática",
-    image_url: "/inacap.webm",
+    image_url: "/inacap.webp",
   },
   {
     id: "2",
     fecha: "Marzo 2019 - Junio 2021",
     nombre: "Inacap Sede Osorno",
     titulo: "Analista Programador",
-    image_url: "/inacap.webm",
+    image_url: "/inacap.webp",
   },
   {
     id: "3",
     fecha: "Marzo 2017 - Diciembre 2018",
     nombre: "Instituto Comercial Osorno",
     titulo: "Programador Nivel Medio",
-    image_url: "/ico.webm",
+    image_url: "/ico.webp",
   },
 ];
 export const tecnologias = [
@@ -249,12 +120,6 @@ export const tecnologias = [
     ),
   },
   {
-    nombre: "Redux",
-    icon: (
-      <IconBrandRedux className="w-12 h-12" aria-label="Next" stroke={1.2} />
-    ),
-  },
-  {
     nombre: "Vite",
     icon: (
       <IconBrandVite className="w-12 h-12" aria-label="Vite" stroke={1.2} />
@@ -281,36 +146,6 @@ export const tecnologias = [
     ),
   },
   {
-    nombre: "C#",
-    icon: (
-      <IconBrandCSharp className="w-12 h-12" aria-label="C#" stroke={1.2} />
-    ),
-  },
-  {
-    nombre: "PHP",
-    icon: <IconBrandPhp className="w-12 h-12" aria-label="PHP" stroke={1.2} />,
-  },
-  {
-    nombre: "Firebase",
-    icon: (
-      <IconBrandFirebase
-        className="w-12 h-12"
-        aria-label="Firebase"
-        stroke={1.2}
-      />
-    ),
-  },
-  {
-    nombre: "Supabase",
-    icon: (
-      <IconBrandSupabase
-        className="w-12 h-12"
-        aria-label="Supabase"
-        stroke={1.2}
-      />
-    ),
-  },
-  {
     nombre: "NodeJs",
     icon: (
       <IconBrandNodejs className="w-12 h-12" aria-label="NodeJs" stroke={1.2} />
@@ -327,8 +162,6 @@ export const tecnologias = [
     ),
   },
 ];
-export const cv =
-  "https://drive.google.com/file/d/1Geo11vo0uYd-1ECh8gW0DoMyNuq_Y6IU/view?usp=sharing";
 export const LINKS = [
     {
       href: "https://github.com/Esteban-Montecinos",
@@ -339,15 +172,5 @@ export const LINKS = [
       href: "https://www.linkedin.com/in/esteban-montecinos/",
       title: "LinkedIn",
       Icon: (props) => <IconBrandLinkedin {...props}/>,
-    },
-    {
-      href: cv,
-      title: "Currículum Vitae",
-      Icon: (props) => <IconFileCv {...props}/>,
-    },
-    {
-      href: "mailto:estebanmontecinos2001@gmail.com",
-      title: "Gmail Esteban Montecinos",
-      Icon: (props) => <IconBrandGmail {...props}/>,
-    },
+    }
   ];
--- src/components/proyectos-list.jsx
+++ src/components/proyectos-list.jsx
@@ -4,12 +4,9 @@ import ProyectoCard from './proyecto-card';
 import { proyectos } from '../../constants';
 export default function ProyectosList() {
   return (
-    <section className='flex flex-col min-h-screen gap-2'>
+    <section className='flex flex-col gap-2'>
         <h2 className="text-3xl font-bold">Proyectos</h2>
-        <p className="text-sm dark:text-zinc-400 text-zinc-700">
-          Aquí hay algunos de mis proyectos, puedes verlos todos en mi <Link href="https://github.com/Esteban-Montecinos" className="underline" target="_blank">GitHub.</Link>
-        </p>
-        <div className='grid grid-cols-1 gap-8 my-8 lg:grid-cols-2'>
+        <div className='grid grid-cols-1 my-8'>
         {proyectos.map((proyecto) => (
             <ProyectoCard key={proyecto.id} {...proyecto}/>
           ))}
